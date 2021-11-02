585c5c4cbc98c9f9aaf73c7a006cc7d30f2e7abf
Sebastien Besson <seb.besson@gmail.com>
2021-11-02 21:26:41 +0000

    Create target directory

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 find * -name *.svg -exec sh -c 'convert $1 -background "rgba(0,0,0,0)" "${1%.*}.png"' sh {} \;
-zip -r idr idr-logos.zip
-zip -r ome ome-logos.zip
-zip -r ome bio-formats ome-files ome-product-logos.zip
+mkdir -p target
+zip -r target/idr-logos.zip idr
+zip -r target/ome-logos.zip ome
+zip -r target/ome-product-logos.zip omero bio-formats ome-files

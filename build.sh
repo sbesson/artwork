f30a55201b91466ee9656089ffea038a68a1426d
Sebastien Besson <seb.besson@gmail.com>
2021-12-16 17:56:37 +0000

    Improve the generation of the OME favicon

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 find * -name *.svg -exec sh -c 'convert -background none $1 "${1%.*}.png"' sh {} \;
-convert -density 384 -background none ome/ome-logomark.svg -define icon:auto-resize ome/ome.ico
+convert -background none ome/ome-logomark.svg -define icon:auto-resize -resize "224x224^" -gravity center -extent 256x256 ome/ome.ico
 mkdir -p target
 zip -r target/idr-logos.zip idr
 zip -r target/ome-logos.zip ome
